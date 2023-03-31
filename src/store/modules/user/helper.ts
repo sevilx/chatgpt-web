9c44321c6774f0d9831f3bd00bcbe8b1d3050598
sevilx <sevilxiong@gmail.com>
2023-03-31 15:52:13 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/sevilx/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'XiongBen',
+      description: '欢迎 <a href="仅限亲朋好友使用，请勿随意传播" class="text-blue-500" target="_blank" >打赏</a>',
     },
   }
 }
